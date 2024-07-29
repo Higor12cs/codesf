978b21e5afe811c28e4ee5e07a72cd71efd47f0a
Higor Carneiro <higor12cs@gmail.com>
2024-07-28 22:37:10 -0300

    fix: remove caching

--- app/View/Components/PostsSection.php
+++ app/View/Components/PostsSection.php
@@ -4,24 +4,22 @@ namespace App\View\Components;
 
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\View\Component;
 
 class PostsSection extends Component
 {
     public function render(): View
     {
-        $posts = Cache::remember('posts', config('cache.cache_time'), function () {
-            return Post::query()
-                ->where('is_published', true)
-                ->where('published_at', '<=', now())
-                ->where(function ($query) {
-                    $query->whereNull('published_until')
-                        ->orWhere('published_until', '>=', now());
-                })
-                ->latest('published_at')
-                ->get();
-        });
+        $posts = Post::query()
+            ->where('is_published', true)
+            ->where('published_at', '<=', now())
+            ->where(function ($query) {
+                $query->whereNull('published_until')
+                    ->orWhere('published_until', '>=', now());
+            })
+            ->latest('published_at')
+            ->take(3)
+            ->get();
 
         return view('components.posts-section', compact('posts'));
     }
